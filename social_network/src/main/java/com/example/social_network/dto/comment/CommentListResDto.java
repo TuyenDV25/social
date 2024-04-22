bf8f4b1436978ab67a3984fd6cea7799d61dc5a4
dovantuyen <dovantuyen@luvina.net>
2024-04-22 17:30:17 +0700

    up

--- social_network/src/main/java/com/example/social_network/dto/comment/CommentListResDto.java
+++ social_network/src/main/java/com/example/social_network/dto/comment/CommentListResDto.java
@@ -2,14 +2,12 @@ package com.example.social_network.dto.comment;
 
 import org.springframework.data.domain.Page;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Getter
+@Setter
 @Builder
 public class CommentListResDto {
 
